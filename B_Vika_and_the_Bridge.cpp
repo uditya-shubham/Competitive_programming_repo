0f378e98a930fc48413a82dec1eaae934da0a750
Uditya Kumar <shubhamkumaar2001@gmail.com>
2024-05-26 12:20:57 +0530

    hgfjytukyg

---
+++ B_Vika_and_the_Bridge.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define int long long
+#define vi vector<int>
+
+
+
+//my Code
+void solve(){
+    int n=1;
+    cin>>n;
+    
+}
+
+
+//main func
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+  //  int Tests = 1;
+  //  cin >> Tests;
+ //   while (Tests--)
+   // {
+        solve();
+  //  }
+    return 0;
+}
+
+//Uditya Yadav here
+
+    
